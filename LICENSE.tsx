6ef2d597319155cef298a9631a444bd92cffbde2
Paras <parasbishnoi012@gmail.com>
2026-07-17 09:53:03 +0530

    Add files via upload

---
+++ LICENSE.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface ├── license                    # mit licenseProps {
+  // Define props here
+}
+
+const ├── license                    # mit license: React.FC<├── license                    # mit licenseProps> = () => {
+  return (
+    <div className="license-mit-license">
+      <h1>├── license                    # mit license Component</h1>
+      {/* Component content */}
+    </div>
+  );
+};
+
+export default ├── license                    # mit license;
